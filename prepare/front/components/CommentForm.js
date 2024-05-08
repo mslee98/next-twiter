975bc7c37ac8e8c90fa483ce054b0e682f694c7b
mslee98 <lms980321@kakao.com>
2024-05-08 18:13:07 +0900

    'comment form style update'

--- prepare/front/components/CommentForm.js
+++ prepare/front/components/CommentForm.js
@@ -9,16 +9,25 @@ const CommentForm = ({post}) => {
     
     const id = useSelector((state) => state.user.me?.id)
 
+    if(id) {
+        console.log("id 있음", id)
+    } else {
+        console.log("id 없음")
+    }
+
     const [commentText, onChangeCommentText]  = useInput('');
+
     const onSubmitComment = useCallback(() => {
-        console.log("ssssssssss");
+        console.log(post.id, commentText)
     }, [commentText])
     
     return (
         <Form onFinish={onSubmitComment}>
             <Form.Item style={{position:'relative', margin: 0 }}>
                 <Input.TextArea value={commentText} onChange={onChangeCommentText} rows={4} />
-                <Button type="primary" htmlType='submit' style={{position: 'absolute',right:0, bottom: -40}}>삐약</Button>
+                <Button type="primary" htmlType='submit' 
+                    style={{position: 'absolute', right:0, bottom: -40, zIndex:2}}
+                    >삐약</Button>
             </Form.Item>
         </Form>
     )
